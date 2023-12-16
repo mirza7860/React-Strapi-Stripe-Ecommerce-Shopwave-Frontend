efd75e93fb153c5a858b8649e70996cfb0dc024b
Mirza Sahil Alli Baig <136656845+mirza7860@users.noreply.github.com>
2023-12-17 04:25:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ const router = createBrowserRouter([
       }
     ],
   },
-],{basename:"https://shopwave-ecommerce.onrender.com/"});
+],{basename:"https://shopwave-ecommerce.onrender.com"});
 
 
 
